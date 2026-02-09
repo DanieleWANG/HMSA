f16e8a783e3e45b0f307bd527b46fa3c8a029707
DanieleWANG <aubreyqxdarb7dlezlpon@gmail.com>
2026-02-09 14:59:33 +0800

    Refactor PreprocessedTimeSeriesDataset class

--- PreprocessedTimeSeriesDataset.py
+++ PreprocessedTimeSeriesDataset.py
@@ -1,21 +1,11 @@
-"""
-预处理的时间序列数据集类
-直接从预处理的 numpy array 加载，避免动态构建
-"""
+
 import pickle
 import numpy as np
 import pandas as pd
 
 
 class PreprocessedTimeSeriesDataset:
-    """从预处理的时间序列数据加载，高效且省内存"""
     def __init__(self, data_path):
-        """
-        Parameters
-        ----------
-        data_path : str
-            预处理的时间序列数据文件路径 (.pkl)
-        """
         with open(data_path, 'rb') as f:
             data_dict = pickle.load(f)
         
@@ -27,7 +17,6 @@ class PreprocessedTimeSeriesDataset:
         return self.length
     
     def __getitem__(self, idx):
-        """直接返回预计算的时间序列数据，非常快"""
         if isinstance(idx, (int, np.integer)):
             return self.data[idx]  # (T, F)
         elif isinstance(idx, (list, np.ndarray, tuple)):
@@ -36,6 +25,6 @@ class PreprocessedTimeSeriesDataset:
             return self.data[int(idx)]
     
     def get_index(self):
-        """返回索引，兼容 qlib 数据集接口"""
         return self.index
+
 
